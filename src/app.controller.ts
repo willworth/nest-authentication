24e9ceb3d1eef35cf319f99d55932e3ad49c1d9c
Will Worth <willworthdev@gmail.com>
2022-10-26 23:00:41 +0200

    passport strategy

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import { Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { LocalAuthGuard } from './auth/local-auth-guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
-  login(): string {
-    return this.appService.getHello();
+  login(@Req() req): any {
+    return req.user;
   }
 
   @Get()
--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
@@ -7,5 +7,15 @@ import { AuthService } from './auth.service';
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authservice: AuthService) {
     super(); // config here
+  }
+
+  async validate(username: string, password: string): Promise<any> {
+    const user = await this.authservice.validateUser(username, password);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    return user;
   }
 }
